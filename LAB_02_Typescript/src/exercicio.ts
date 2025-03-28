07b3c0882aeba2d63001fda088b2fecc04f40bf4
MatheusIFSP <eudolly19@gmail.com>
2025-03-28 20:35:47 -0300

    ola

---
+++ LAB_02_Typescript/src/exercicio.ts
@@ -0,0 +1,3 @@
+let idades01: number[] = [ 10, 15, 20, 8, 17, 25 ]
+
+for 
---
+++ LAB_02_Typescript/src/variaveisTipos.ts
@@ -0,0 +1,14 @@
+let nome : string = " Jo ã o ";
+let idade : number = 25;
+let ativo : boolean = true ;
+
+let dado : any = 10;
+let vazio : null = null ;
+let indefinido : undefined = undefined ;
+// Tipos literais ( valores espec í ficos )
+let situacao : " ativo " | " inativo " = " ativo "; // S ó aceita esses dois valores
+
+// Uni ã o de tipos ( Union Type )
+let idadeOuNulo : number | null = null ;
+
+console.log (` Nome : $ { nome } , Idade : $ { idade } , Ativo : $ { ativo }`) ;
