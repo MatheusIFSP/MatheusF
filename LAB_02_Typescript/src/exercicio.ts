f205af8867ec1be52d92a4e3e4943904e3fc73d8
MatheusIFSP <eudolly19@gmail.com>
2025-04-03 16:46:57 -0300

    dia 03 de abril

--- LAB_02_Typescript/src/exercicio.ts
+++ LAB_02_Typescript/src/exercicio.ts
@@ -1,1 +1,42 @@
+                            // Exercício 1//
+
 let idades01: number[] = [ 10, 15, 20, 8, 17, 25 ]
+
+for (let idade of idades) {
+    if (idade <12) {
+    console.log(`${idade} anos - criança`);
+} else if (idade >= 12 && idade < 18) {
+    console.log(`${idade} anos - adolescente`);
+} else {
+    console.log(`${idade} anos - adulto`);
+}
+}
+
+                            // Exercício 2//
+    
+const nomes0: string[] = ["Carlos", "Ana", "Pedro", "Mariana"];
+const index = nomes.indexOf("Pedro");
+if(index !== -1) {
+    nomes0.splice(index, 1);
+}
+nomes0.push("João");
+nomes0.unshift("Beatriz");
+console.log("Lista final:", nomes0);
+console.log("Total de nomes:", nomes0.length);
+
+                            // Exercício 3//
+
+let ListaMista: (number|string)[] = [1, "Maçã", 2, "Banana", 3, "Laranja"]
+let numeros: [] = [];
+let frutas: [] = [];
+for (let item of ListaMista) {
+    if (typeof item === "number") {
+        numeros.push(number);
+    } else if (typeof item === "string") {
+        frutas.push(string);
+    }
+}
+console.log("Números:", numeros);
+console.log("Frutas:", frutas);
+
+                            // Exercício 2.1//
--- LAB_02_Typescript/src/variaveisTipos.ts
+++ LAB_02_Typescript/src/variaveisTipos.ts
@@ -1,4 +1,4 @@
-let nome : string = " Jo ã o ";
+let nome : string = " João ";
 let idade : number = 25;
 let ativo : boolean = true ;
 
