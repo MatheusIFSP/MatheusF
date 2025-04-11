898dfdbf9122337cae3144b80824f7048bd63f9b
MatheusIFSP <eudolly19@gmail.com>
2025-04-11 16:54:07 -0300

    dia 10 de abril

--- LAB_02_Typescript/src/exercicio.ts
+++ LAB_02_Typescript/src/exercicio.ts
@@ -182,4 +182,81 @@ type Tarefa = {
     concluida: Boolean;
 };
 
-function executarTarefas[] = Tarefa[]
+                            //Exercício 2 - LAB 07//
+
+class Livro {
+    titulo: string
+    autor: string
+    anoPublicacao: number
+    editora: string
+                            
+                            
+    constructor(titulo: string, autor: string, anoPublicacao: number, editora: string){
+    this.titulo = titulo
+    this.autor = autor
+    this.anoPublicacao = anoPublicacao
+    this.editora = editora
+    }
+                            
+                            
+    exibirDados(){
+        console.log(`Título: ${this.titulo}
+            Autor: ${this.autor}
+            Ano de Publicação: ${this.anoPublicacao}
+            Editora: ${this.editora}`)
+    }
+}
+                            
+                            
+                            //Exercício 2 - LAB 07//
+                            
+                            
+                            
+                            
+                            
+                            
+                            //Exercício 3 - LAB 07//
+                            
+                            
+class Usuario {
+    nome: string
+    email: string
+                            
+                            
+    constructor(nome: string, email: string){
+    this.nome = nome
+    this.email = email
+    }
+    boasVindas(){
+        console.log(`Bem-vindo(a), ${this.nome}!
+            Seu email é ${this.email}.`)
+    }
+}
+                            
+                            
+const boasvindas = new Usuario("Matheus", "matheus.f.rodrigues2006@gmail.com")
+boasvindas.boasVindas()
+                            
+                            
+                            //Exercício 5 - LAB 07//
+                            
+                            
+export class Produtos {
+    nome: string
+    preco: number
+                            
+                            
+    constructor(nome: string, preco: number) {
+    this.nome = nome
+    this.preco = preco
+    }
+    mostrarInformacoes(): void{
+        console.log(`Nome: ${this.nome}
+            Preço: ${this.preco}`)
+    }
+}
+                            
+                            
+const prod = new Produtos("Notebook", 3500)
+prod.mostrarInformacoes()
+                            
