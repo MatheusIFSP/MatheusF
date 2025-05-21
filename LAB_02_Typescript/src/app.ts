c9d0e5630c5d2078bc300059b67a4705d2e467c0
MatheusIFSP <eudolly19@gmail.com>
2025-05-21 15:33:48 -0300

    continuação 16 de maio

--- LAB_02_Typescript/src/app.ts
+++ LAB_02_Typescript/src/app.ts
@@ -25,19 +25,46 @@ function novoJogador (req: Request, res: Response): void {
     try {
         let data: any = req.body
         const id = jogadores.length + 1;
-    
-    const novoJogador = new jogador (data.id, data.nome, data.cpf, data.email, data.telefone, data.posicao)
-    jogadores.push(novoJogador)
-    res.status(200).json(jogador)
+        const novoJogador = new jogador(data.id, data.nome, data.cpf, data.email, data.telefone, data.posicao)
+        jogadores.push(novoJogador)
+        res.status(200).json(novoJogador)
     } catch (e: unknown) {
     res.status(400).json ({ Message: (e as Error).message })
+    }
 }
 
 function atualizarJogador (req: Request, res: Response): void {
     try{
-        let id = req.params.id
-        let data: any = req.body
-        
+        let id = Number(req.params.id);
+        let data: any = req.body;
+        for (let i = 0; i < jogadores.length; i++) {
+            if (jogadores[i].id === id) {
+                jogadores[i].nome = data.nome
+                jogadores[i].cpf = data.cpf
+                jogadores[i].email = data.email
+                jogadores[i].telefone = data.telefone
+                jogadores[i].posicao = data.posicao
+                res.status(200).json({ Message: "Jogador atualizado com sucesso", jogador: jogadores[i]});
+                return;
+            }
+        }
+        res.status(404).json({ Message: "Jogador não encontrado" });
+    } catch (e: unknown) {
+        res.status(400).json ({ Message: (e as Error).message })
+    }
+}
+
+function removerJogador (req: Request, res: Response): void {
+    try {
+        let id = Number(req.params.id);
+        for (let i = 0; i < jogadores.length; i++) {
+            if (jogadores[i].id === id) {
+                jogadores.splice(i, 1);
+                res.status(200).json({ Message: "Jogador removido com sucesso" });
+                return;
+            }
+        }
+        res.status(404).json({ Message: "Jogador não encontrado" });
     } catch (e: unknown) {
         res.status(400).json ({ Message: (e as Error).message })
 }
@@ -46,6 +73,5 @@ app.get('/api/jogadores', listarJogadores)
 app.get('/api/jogadores/:id', filtraJogador)
 app.post('/api/jogadores', novoJogador)
 app.put('/api/jogadores/:id', atualizarJogador)
-
-}
+app.delete('/api/jogadores/:id', removerJogador)
 }
